dbb89714458356886d94e14e20477dc50831d615
Otsu <sura1sura1@yahoo.co.jp>
2018-04-05 09:09:37 +0900

    プレイヤーの基本移動を実装

---
+++ Assets/Otsu/Script/Player/PlayerMove.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    Transform player;
+    Rigidbody playerRb;
+
+    public float speed = 0.1f;
+    public float jump = 250;
+
+    bool jumpFlg = true;
+    // Use this for initialization
+    void Start()
+    {
+        player = this.gameObject.GetComponent<Transform>();
+        playerRb = this.gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+
+        Debug.Log("jumpFlgは" + jumpFlg);
+    }
+
+    void Move()
+    {
+
+        //  左移動
+        if (Input.GetKey(KeyCode.A))
+        {
+            player.Translate(Vector2.left * speed);
+        }
+
+        //  右移動
+        if (Input.GetKey(KeyCode.D))
+        {
+            player.Translate(Vector2.right * speed);
+        }
+
+        //ジャンプ
+        if (Input.GetKey(KeyCode.Space) && jumpFlg == true)
+        {
+            playerRb.AddForce(Vector2.up * jump);
+            jumpFlg = false;
+        }
+    }
+
+    void OnTriggerEnter(Collider floor)
+    {
+        if (floor.gameObject.tag == "Floor")
+        {
+            jumpFlg = true;
+        }
+    }
+}
